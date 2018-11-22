a824b341a4ab86d3721eb5f38920d137624b474e
mateusz <piecuch@stethome.com>
2018-11-22 18:02:36 +0100

    The 3n+1 Problem

---
+++ chapter_1/the_3n_+_1_problem/prob_100.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <vector>
+#include <map>
+
+std::map<int, int> res_map;
+
+void calc_3n_plus_1(int x) {
+	std::vector<int> stack;
+	while(!res_map[x] && x!=1) {
+		stack.push_back(x);
+		if(x%2==0) 
+			x=x>>1;
+		else
+			x=x*3+1;
+	}
+	int last=x;
+	for(int i=stack.size()-1;i>=0;i--) {
+		res_map[stack[i]]=res_map[last]+1;
+		last=stack[i];
+	}
+}
+
+int main() {
+	int i, j;
+	res_map[1]=1;
+	while(scanf("%d%d",&i,&j)!=EOF) {
+		int result=0;
+		int upper_bound=std::max(i,j);
+		for(int x=std::min(i,j);x<=upper_bound;x++) {
+			calc_3n_plus_1(x);
+			result=std::max(result, res_map[x]);
+		}
+		printf("%d %d %d\n",i,j,result);
+	}	
+
+	return 0;
+}
